ee819b88c998cdc4a1b9e1382453d5dcd03dff5e
graupv <38445832+graupv@users.noreply.github.com>
2020-06-05 23:47:36 -0600

    things

--- initializer.py
+++ initializer.py
@@ -1,6 +1,10 @@
 #   desde venv
 
 from django.contrib.auth.models import User 
-from track.models import track
+from track.models import Track
 
-# user = User.objects.create('')
+user = User.objects.create(username='usuario', password='1234', email=em@il)
+user.save()
+
+track = Track(id=1, name='my song', username=user.username, lyrics='words', key='A', version=1)
+track.save()
