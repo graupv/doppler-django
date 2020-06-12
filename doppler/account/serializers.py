e127297dfe62c21fc13c5ec820836f99c0a3bde7
graupv <38445832+graupv@users.noreply.github.com>
2020-06-12 17:32:25 -0600

    account creation and edit views with permissions and responses

--- doppler/account/serializers.py
+++ doppler/account/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth import get_user_model 
 # If used custom user model
 
--- doppler/account/views.py
+++ doppler/account/views.py
@@ -1,5 +1,8 @@
-from rest_framework import permissions
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+
 from django.contrib.auth import get_user_model 
 from django.contrib.auth.models import User 
 from permissions.services import APIPermissionClassFactory
@@ -10,26 +13,60 @@ from account.serializers import UserSerializer
 class CreateUserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     model = get_user_model()
+    parser_classes = [JSONParser]
     serializer_class = UserSerializer
-    permission_classes = (
-        APIPermissionClassFactory(
-            name='AccountPermission',
-            permission_configuration={
-                'base': {
-                    'create': permissions.AllowAny,
-                    #   anyone can sign up
-                    # 'list': lambda user, req: user.is_authenticated,
-                    #   list -> user.is_admin o algo despues
+    # permission_classes = (
+    #     APIPermissionClassFactory(
+    #         name='AccountPermission',
+    #         permission_configuration={
+    #             'base': {
+    #                 'create': permissions.AllowAny,
+    #                 #   anyone can sign up
+    #                 # 'list': lambda user, req: user.is_authenticated,
+    #                 #   list -> user.is_admin o algo despues
                     
-                },
-                'instance': {
-                    #   empty to avoid user requests
-                    # 'retrieve': evaluar_user,
-                    # 'destroy': evaluar_user,
-                    # 'update': evaluar_user,
-                    # 'partial_update': evaluar_user,
-                    # # 'create': evaluar_user,
-                }
-            }
-        ),
-    )
+    #             },
+    #             'instance': {
+    #                 #   empty to avoid user requests
+    #                 # 'retrieve': evaluar_user,
+    #                 # 'destroy': evaluar_user,
+    #                 # 'update': evaluar_user,
+    #                 # 'partial_update': evaluar_user,
+    #                 # # 'create': evaluar_user,
+    #             }
+    #         }
+    #     ),
+    # )
+    
+    # @parser_classes([JSONParser])
+    @action(detail=False, url_path='create', methods=['POST'], permission_classes=[permissions.AllowAny])
+    def create_user(self, request):
+
+        data = self.request.data
+        #   get querydict object with Body data
+        user = data.get('username')
+        pw = data.get("password")
+        email = data.get('email')
+
+        #   Wrap in try and return errors
+        try:
+            User.objects.create_user(user, email, pw)
+            print('User created')
+            return Response(status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print('Create user exception\n', e)
+            return Response({'Result': 'Recevied but unable to create user'}, status=status.HTTP_202_ACCEPTED)
+        
+        
+
+    @action(detail=True, url_path='edit', methods=['POST'], permission_classes=[permissions.IsAuthenticated])
+    def modify_user(self, request, pk=None):
+
+        user = self.request.user
+        print(user, user.id)
+        print(pk)
+        if pk != user.id:
+            #   user cannot change a different user's details
+            #   or simply wrong url
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return Response({'Status': 'Modified something'}, status=status.HTTP_200_OK)
